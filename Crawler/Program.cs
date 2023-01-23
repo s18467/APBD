3489951535bc256ec41d1a80d816eac2a366f974
s18467 <s18467@pjwstk.edu.pl>
2023-01-23 12:04:56 +0100

    #ex1 #done

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -4,34 +4,49 @@ namespace Crawler
 {
     public class Program
     {
-        //private const string url = "https://pastebin.com/archive";
-        private const string pattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+        private const string urlPattern = @"^(http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?$";
+        private const string mailPattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello! Looking for emails listed on provided URL. Please wait...");
-            if (args == null || args.Length == 0)
-            {
-                Console.WriteLine("Please give URL as a parameter.");
-                return;
-            }
-            var url = args[0];
-            using var client = new HttpClient();
             try
             {
-                var html = await client.GetStringAsync(url);
-                var emailMatches = Regex.Matches(html, pattern);
-
-                if (emailMatches.Count == 0)
+                if (args == null || args.Length == 0)
                 {
-                    Console.WriteLine("\nNo email addresses found.");
-                    return;
+                    throw new ArgumentNullException("Please give URL as a parameter.");
                 }
 
-                Console.WriteLine($"\nEmail addresses found [{emailMatches.Count}]:");
-                foreach (Match match in emailMatches)
+                var url = args[0];
+                var urlMatch = Regex.IsMatch(url, @"^(http|https)://", RegexOptions.IgnoreCase);
+                if (!urlMatch)
                 {
-                    Console.WriteLine(match.Value);
+                    throw new ArgumentException("Podaj poprawny URL");
+                }
+
+                Console.WriteLine("Prosze czekac ...");
+                using var client = new HttpClient(); // Automatyczne uzycie Dispose() po zakonczeniu bloku using (automatycznie wykrywa)
+
+                try
+                {
+                    var html = await client.GetStringAsync(url);
+                    var emailMatches = Regex.Matches(html, mailPattern);
+
+                    if (emailMatches.Count == 0)
+                    {
+                        Console.WriteLine("\nNie znaleziono adresów email.");
+                        return;
+                    }
+
+                    var emails = emailMatches.Select(match => match.Value).Distinct().ToList();
+                    Console.WriteLine($"\nEmail addresses found [{emails.Count}]:");
+                    foreach (var email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Błąd w czasie pobierania strony");
                 }
             }
             catch (Exception e)
